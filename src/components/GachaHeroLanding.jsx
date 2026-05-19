e5843ac5e7c10f8caff5e8b57567d41499ab2503
Blue <aaronlam709@gmail.com>
2026-05-19 11:28:40 +0800

    front page

--- src/components/GachaHeroLanding.jsx
+++ src/components/GachaHeroLanding.jsx
@@ -1,4 +1,5 @@
 import WordSphereNav from "./WordSphereNav";
+import TypographySphereCanvas from "./TypographySphereCanvas";
 
 export default function GachaHeroLanding({ onNavigate, onEnter }) {
   return (
@@ -20,6 +21,7 @@ export default function GachaHeroLanding({ onNavigate, onEnter }) {
         <button type="button" onClick={onEnter} className="glass-enter-btn px-8 py-3">
           Enter Diorama
         </button>
+        <TypographySphereCanvas className="mt-6" />
       </footer>
     </div>
   );
