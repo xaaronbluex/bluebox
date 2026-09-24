e451f7b448e101ef1443efd87015cfce436f3bfd
Blue <aaronlam709@gmail.com>
2026-09-24 13:07:37 +0800

    Raise WordSphere closer under landing title
    Move absolute anchor from 40% to 30% of the content row so the
    globe sits higher with a smaller gap under EARTH'S DOCUMENTATION.

--- src/components/GachaHeroLanding.jsx
+++ src/components/GachaHeroLanding.jsx
@@ -14,9 +14,9 @@ export default function GachaHeroLanding({ onNavigate }) {
         </h1>
       </header>
 
-      {/* Anchor sphere center in the blank under the title (~40% of content row) */}
+      {/* Anchor sphere center higher under the title (~30% of content row) */}
       <div className="relative z-10 min-h-0 overflow-hidden">
-        <div className="absolute left-1/2 top-[40%] w-full -translate-x-1/2 -translate-y-1/2 px-3">
+        <div className="absolute left-1/2 top-[30%] w-full -translate-x-1/2 -translate-y-1/2 px-3">
           <WordSphereNav onNavigate={onNavigate} />
         </div>
       </div>
